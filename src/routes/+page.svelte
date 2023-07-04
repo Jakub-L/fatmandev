f2a4666c54f32a1600c1a7b1ef8ae80fff1d5e24
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2023-07-04 15:14:55 +0100

    First deployment (#1)
    * Add static site generation
    * Add redirection to slash-24
    * Basic setup with /24 link
    
    ---------
    
    Co-authored-by: Jakub <jakub.lech@sennen.tech>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-4xl text-red-500">fatman.dev</h1>
+
+<a href="/24" class="underline hover:scale-125 origin-center">/24</a>
