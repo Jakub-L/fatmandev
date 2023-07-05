d44fed1b5b8292a69c93210d66da789c6b03d2f3
Jakub <jakub.lech@sennen.tech>
2023-07-05 06:37:26 +0100

    Convert /24 link to card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
-<h1 class="text-4xl text-red-500">fatman.dev</h1>
+<script>
+	import Card from '../components/card.svelte';
+</script>
 
-<a href="/24" class="underline hover:scale-125 origin-center">/24</a>
+<h1 class="text-4xl text-red-500">fatman.dev</h1>
+<Card
+	title="/24 • Subnetting calculator"
+	imgSrc="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
+	href="/24"
+	linkText="fatman.dev/24"
+>
+	Web-based subnetting tool. It explains the principles behind IP addresses and subnet masks, and
+	allows interactive calculations, showing bit-by-bit changes. Built using pure HTML, CSS and JS,
+	fully accessible, responsive, with a dark and light mode.
+</Card>
