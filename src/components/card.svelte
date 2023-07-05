1b18fce2d3aee78b7f22c33c294e45177948ec43
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2023-07-05 22:47:19 +0100

    Update card component

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	export let title: string;
-	export let imgSrc: string;
+	export let imgSources: { src: string; type: string }[];
 	export let href: string;
 	export let linkText: string = href;
 </script>
@@ -10,7 +10,17 @@
 	target="_blank"
 	class="m-4 flex w-1/2 max-w-2xl gap-4 rounded-lg border border-red-700 bg-red-950 bg-opacity-50 p-4 hover:shadow-lg hover:shadow-red-700 hover:ring-2 hover:ring-red-700"
 >
-	<img src={imgSrc} alt={title} class="h-40 w-40 object-cover" />
+	<div class="w-full">
+		<picture>
+		{#each imgSources as { src, type }, i}
+			{#if i !== 0}
+				<source srcset={src} {type} />
+			{:else}
+				<img class="w-40 h-40"  {src} alt={title} />
+			{/if}
+		{/each}
+	</picture>
+	</div>
 	<div class="flex grow flex-col gap-2">
 		<h2 class="text-xl font-bold lowercase">{title}</h2>
 		<p class="grow text-sm"><slot /></p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,10 @@
 <h1 class="text-4xl text-red-500">fatman.dev</h1>
 <Card
 	title="/24 • Subnetting calculator"
-	imgSrc="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
+	imgSources={[
+		{ src: 'project-images/slash-24.webp', type: 'image/webp' },
+		{ src: 'project-images/slash-24.png', type: 'image/png' },
+	]}
 	href="/24"
 	linkText="fatman.dev/24"
 >
