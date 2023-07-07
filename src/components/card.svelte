939d8f89fb09c05002227966f3a76d144d20fe86
Jakub <jakub.lech@sennen.tech>
2023-07-07 07:04:44 +0100

    Add repo links, change description from slot to prop for easier iterative generation

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
+	type Link = { href: string; text?: string };
+
 	export let title: string;
 	export let imgSources: { src: string; type: string }[];
-	export let href: string;
-	export let linkText: string = href;
+	export let repo: Link | null = null;
+	export let live: Link;
+	export let description: string;
 </script>
 
-<a
-	{href}
-	target="_blank"
+<div
 	class={`m-4 grid w-full max-w-2xl auto-rows-auto grid-cols-4 items-center gap-4
-		rounded-lg border border-red-700 bg-red-950 bg-opacity-50 p-4 
-		hover:shadow-lg hover:shadow-red-700 hover:ring-2 hover:ring-red-700`}
+		rounded-lg border border-red-700 bg-red-950 bg-opacity-50 p-4`}
 >
 	<div class="w-full xs:row-span-3">
 		<picture class="w-full">
@@ -24,6 +24,24 @@
 		</picture>
 	</div>
 	<h2 class="col-span-3 text-xl font-bold lowercase">{title}</h2>
-	<p class="col-span-4 text-sm xs:col-span-3"><slot /></p>
-	<span class="col-span-4 border-t border-red-700 pt-1 text-right text-xs">{linkText}</span>
-</a>
+	<p class="col-span-4 text-sm xs:col-span-3">{description}</p>
+	<div class="col-span-4 flex items-center justify-between border-t border-red-700 pt-1 text-sm xs:col-span-3">
+		{#if repo}
+			<a
+				href={repo.href}
+				class={`flex items-center justify-start gap-2 rounded-full px-1 underline decoration-dotted 
+					hover:scale-105 focus-visible:outline-dotted focus-visible:outline-2 focus-visible:outline-red-200`}
+			>
+				<img src="/ui/github-logo.svg" alt="Github repository" class="h-5 w-5" />
+				{repo.text ?? repo.href}
+			</a>
+		{/if}
+		<a
+			href={live.href}
+			class={`flex items-center justify-end gap-2 rounded-full px-1 underline decoration-dotted 
+				hover:scale-105 focus-visible:outline-dotted focus-visible:outline-2 focus-visible:outline-red-200`}
+		>
+			{live.text ?? live.href}
+		</a>
+	</div>
+</div>
