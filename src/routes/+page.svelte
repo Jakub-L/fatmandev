6e2b025d161675cdfa2cbc31e374c1713cbaaec8
Jakub L <47220449+Jakub-L@users.noreply.github.com>
2023-07-08 22:32:43 +0100

    Add fatman.dev as a project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,19 @@
 				{ src: 'project-images/slash-24.png', type: 'image/png' }
 			],
 			live: { href: '/24', text: 'fatman.dev/24' },
-			repo: { href: 'https://github.com/Jakub-L/slash-24', text: 'Github' },
+			repo: { href: 'https://github.com/Jakub-L/slash-24', text: 'Github repository' },
 			description:
 				'Web-based subnetting tool. It explains the principles behind IP addresses and subnet masks, and allows interactive calculations, showing bit-by-bit changes. Built using pure HTML, CSS and JS, fully accessible, responsive, with a dark and light mode.'
+		},
+		{
+			title: 'fatman.dev',
+			imgSources: [
+				{ src: 'project-images/fatmandev.webp', type: 'image/webp' },
+				{ src: 'project-images/fatmandev.png', type: 'image/png' }
+			],
+			live: { href: '/', text: 'fatman.dev' },
+			repo: { href: 'https://github.com/Jakub-L/fatmandev', text: 'Github repository' },
+			description: 'This site! Simple homepage for listing all my projects. Built using SvelteKit and TailwindCSS.'
 		}
 	];
 </script>
